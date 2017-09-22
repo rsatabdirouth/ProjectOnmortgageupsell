8f9c2dffd64a9aba9cd59d268a1ee66e8e53e419
Satabdi Routh <satabdi@emedia.com>
2017-09-22 13:00:51 +0530

    firstcommit

---
+++ ProjectOnUpsell/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectOnUpsell.Models;
+
+namespace ProjectOnUpsell.Controllers
+{
+    public class HomeController : Controller
+    {
+        UpsellEntities3 _db = new UpsellEntities3();
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Requirement model = new Requirement();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Requirement model)
+
+        {
+            _db.Requirements.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("SecondIndex", "Second");
+        }
+    }
+}
